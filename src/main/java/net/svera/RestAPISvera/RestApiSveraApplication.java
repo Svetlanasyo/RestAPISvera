4814cf2d47686662218af39e74e2894d0d93b3bb
Svetlana <svetlana.syo@gmail.com>
2019-11-03 20:35:45 +0100

    Initial commit

---
+++ src/main/java/net/svera/RestAPISvera/RestApiSveraApplication.java
@@ -0,0 +1,13 @@
+package net.svera.RestAPISvera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSveraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiSveraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/svera/RestAPISvera/RestApiSveraApplicationTests.java
@@ -0,0 +1,13 @@
+package net.svera.RestAPISvera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSveraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
